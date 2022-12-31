44a55e803c9428b9e76166d01a19ac7ebcf6fe51
Theron Joe <theronj@joetwebdev.io>
2022-12-31 03:28:10 -0700

    added a prompt package, basic setup, will continue on.

--- main.go
+++ main.go
@@ -1,6 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"github.com/AlecAivazis/survey/v2"
+)
+
+var question = []*survey.Question{
+	{
+		Name:      "cipher",
+		Prompt:    &survey.Input{Message: "Which cipher would you like to use?"},
+		Validate:  survey.Required,
+		Transform: survey.Title,
+	},
+}
+
 func main() {
+	answers := struct {
+		Cipher string
+	}{}
+
+	err := survey.Ask(question, &answers)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("%s name", answers.Cipher)
 	// v1
 	// prompt for input
 	// print to file
