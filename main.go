a444d8eff4123eea64d2014dfb1d9b8b067ccabb
Theron Joe <theronj@joetwebdev.io>
2023-01-02 03:09:31 -0700

    added prompts for recieve input for selecting cipher and value to encrypt.

--- main.go
+++ main.go
@@ -2,30 +2,47 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"github.com/AlecAivazis/survey/v2"
 )
 
-var question = []*survey.Question{
-	{
-		Name:      "cipher",
-		Prompt:    &survey.Input{Message: "Which cipher would you like to use?"},
-		Validate:  survey.Required,
-		Transform: survey.Title,
-	},
+// var question = []*survey.Question{
+// 	{
+// 		Name:      "cipher",
+// 		Prompt:    &survey.Input{Message: "Which cipher would you like to use?"},
+// 		Validate:  survey.Required,
+// 		Transform: survey.Title,
+// 	},
+// }
+
+var question = &survey.Select{
+	Message: "Which cipher would you like to use?",
+	Options: []string{"caeser", "vigenere"},
+} 
+
+var prompt = &survey.Input{
+	Message: "Enter your value to encrypt.",
 }
 
 func main() {
-	answers := struct {
-		Cipher string
-	}{}
+	var cipher string = ""
+	var value string = ""
 
-	err := survey.Ask(question, &answers)
+	err := survey.AskOne(question, &cipher)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
-	fmt.Printf("%s name", answers.Cipher)
+	survey.AskOne(prompt, &value)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	value = strings.ToLower(value)
+
+	fmt.Println("Cipher: ", cipher, "\nValue: ", value)
 	// v1
 	// prompt for input
 	// print to file
