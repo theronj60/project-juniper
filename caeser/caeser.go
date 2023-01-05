02bf05ee49441651c63d31ea7911fa864d149d1e
Theron Joe <theronj@joetwebdev.io>
2023-01-05 12:56:29 -0700

    set up for loop to read rune shift 3 spaces and convert to string, then add to new array.

--- caeser/caeser.go
+++ caeser/caeser.go
@@ -4,7 +4,12 @@ import "fmt"
 
 // read from text file
 // should encrypt or decrypt text
-// convert lines to runes
+// convert lines to runes?
+// take a single letter and shift it right 3 spots in the alphabet to encrpyt
+// reverse it with decrypting
+
+// create a variable (array) to convert spaces to a random but predefined character
+// var spaceChar string = {}array
 
 func readFile() {
 	//
@@ -16,10 +21,18 @@ func writeFile() {
 
 func Encrypt(str string, method string) string {
 	if method == "file" {
-		fmt.Println("encrypt caeser file")
-		//
+		// for i, r := range str {
+		// 	fmt.Println("encrypt caeser file")
+		// }
+		// needs to read file
+		// then loop through string
 	} else {
-		//
+		var encryptedTxt []string
+		for _, r := range str {
+			encryptedTxt = append(encryptedTxt, string(r + 3))
+		} 
+		fmt.Println(encryptedTxt)
+		// needs to loop through string
 		fmt.Println("encrypt caeser text")
 	}
 	return "string encrypt"
